40792ce9f5f0ee4a8490c035e16fcc989c814c29
Desipeli <mr.desipeli@gmail.com>
2023-02-16 14:51:21 +0200

    development = test

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 let PORT = process.env.PORT
 let SECRET = process.env.SECRET
 let MONGO_URL =
-  process.env.NODE_ENV === 'test'
+  process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development'
     ? process.env.MONGO_URL_TEST
     : process.env.MONGO_URL
 let NODE_ENV = process.env.NODE_ENV
