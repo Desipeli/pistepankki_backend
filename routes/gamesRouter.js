92eef89f1c419280374506990ee5e7ff8855dff5
Desipeli <mr.desipeli@gmail.com>
2023-02-24 20:27:40 +0200

    moved logic to services

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const config = require('../utils/config')
-const User = require('../models/user')
-const Game = require('../models/game')
 const mongoose = require('mongoose')
 const router = express.Router()
 const getDecodedToken = require('../services/tokenService')
+const {
+  getGames,
+  addGame,
+  isDeleteGameAuthorized,
+  deleteGame,
+  deleteAllGames,
+} = require('../services/gameService')
+const { isAdmin, adminRequired } = require('../services/userService')
 
 const timeouts = {}
+const gamePostingTimeout = async (id) => {
+  if (config.NODE_ENV !== 'test') {
+    const timeOutId = setTimeout(() => {
+      delete timeouts[id]
+    }, 5000)
+    if (id in timeouts) {
+      clearTimeout(timeouts[id])
+      timeouts[id] = timeOutId
+      throw {
+        name: 'TooManyRequests',
+        message: 'wait 5s before trying again',
+      }
+    }
+    timeouts[id] = timeOutId
+  }
+}
 
 router.get('/', async (req, res) => {
   const parameters = {}
@@ -26,171 +48,37 @@ router.get('/', async (req, res) => {
       $lte: decodeURIComponent(req.query.dateto),
     }
   }
-  const allGames = await Game.find(parameters)
-    .populate('players', {
-      games: 0,
-      __v: 0,
-    })
-    .populate('sport', { __v: 0 })
-    .populate('winners', { __v: 0, games: 0, email: 0 })
-    .populate('submitter', { __v: 0, games: 0, email: 0 })
-  res.json(allGames)
+  const allGames = await getGames(parameters)
+  res.status(200).json(allGames)
 })
 
 router.post('/', async (req, res) => {
   const decodedToken = getDecodedToken(req)
-  if (config.NODE_ENV !== 'test') {
-    const timeOutId = setTimeout(() => {
-      delete timeouts[decodedToken.id]
-    }, 5000)
-    if (decodedToken.id in timeouts) {
-      clearTimeout(timeouts[decodedToken.id])
-      timeouts[decodedToken.id] = timeOutId
-      throw {
-        name: 'TooManyRequests',
-        message: 'wait 5s before trying again',
-      }
-    }
-    timeouts[decodedToken.id] = timeOutId
-  }
+  await gamePostingTimeout(decodedToken.id)
 
   const data = req.body
-  const players = {}
-  if (!data.players.includes(decodedToken.id)) {
-    throw {
-      name: 'Forbidden',
-      message: 'You can only post games where you participated',
-    }
-  }
-  for await (const [index, player] of data.players.entries()) {
-    if (player in players) {
-      throw {
-        name: 'Custom',
-        message: 'duplicate player',
-      }
-    }
-    players[player] = await User.findById(player)
-    if (players[player] === null) {
-      throw {
-        name: 'Custom',
-        message: `player ${index + 1} not in database`,
-      }
-    }
-  }
-
-  if (data.rounds.length === 0)
-    throw {
-      name: 'ValidationError',
-      message: 'can not save empty match',
-    }
-  const scores = Array(data.players.length).fill(0)
-
-  // Check rounds(s)
-
-  for (const [roundIndex, round] of data.rounds.entries()) {
-    if (round.length !== data.players.length) {
-      throw {
-        name: 'Custom',
-        message: `Incorrect amount of players in round ${roundIndex + 1}`,
-      }
-    }
-    for (const [scoreIndex, score] of data.rounds[roundIndex].entries()) {
-      if (!isNaN(score)) scores[scoreIndex] += Number(score)
-      else
-        throw {
-          name: 'Custom',
-          message: `malformatted score round ${roundIndex + 1} player ${
-            scoreIndex + 1
-          }`,
-        }
-    }
-  }
-
-  const winners = []
-  const maxScore = scores.reduce((a, b) => Math.max(a, b), 0)
-  for (const [index, value] of scores.entries()) {
-    if (value === maxScore) {
-      winners.push(data.players[index])
-    }
-  }
-
-  // Create and save game. Update users
-  const newGame = new Game({
-    players: data['players'],
-    rounds: data['rounds'],
-    sport: data['sport'],
-    winners: winners,
-    date: Date.now(),
-    approved: data['players'].length > 1 ? false : true,
-    submitter: mongoose.Types.ObjectId(decodedToken.id),
-    approvedBy: mongoose.Types.ObjectId(decodedToken.id),
-  })
-
-  const savedGame = await newGame.save()
-
-  if (savedGame) {
-    for (let userId of Object.keys(players)) {
-      const user = await User.findOne({ _id: userId })
-      if (user['games']) {
-        await User.findByIdAndUpdate(
-          userId,
-          {
-            games: [...user['games'], savedGame['_id']],
-          },
-          { runValidators: true, context: 'query' }
-        )
-      } else {
-        await User.findByIdAndUpdate(
-          userId,
-          { games: [savedGame['_id']] },
-          { runValidators: true, context: 'query' }
-        )
-      }
-    }
-  }
-
+  const savedGame = await addGame(data, decodedToken)
   res.status(201).json(savedGame).end()
 })
 
 router.get('/:id', async (req, res) => {
-  const game = await Game.findById(req.params.id)
-    .populate('players', { games: 0, __v: 0, email: 0 })
-    .populate('sport', { __v: 0 })
-    .populate('winners', { __v: 0, games: 0, email: 0 })
-    .populate('submitter', { __v: 0, games: 0, email: 0 })
-  if (game) {
-    res.status(200).json(game)
-  }
-  res.status(404).end()
+  const game = await getGames({ _id: req.params.id })
+  res.status(200).json(game)
 })
 
 router.delete('/:id', async (req, res) => {
   // Delete match from games and from all connected users
 
   const decodedToken = getDecodedToken(req)
-  const game = await Game.findById(req.params.id)
-  if (game.submitter.toString() !== decodedToken.id)
-    throw {
-      name: 'Authorization',
-      message: 'You did not submit this game',
-    }
-  const deletedMatch = await Game.findByIdAndDelete(req.params.id)
-  const players = deletedMatch.players
-
-  for await (const userId of players) {
-    const user = await User.findById(userId)
-    const filteredGames = user.games.filter(
-      (game) => !game.equals(mongoose.Types.ObjectId(req.params.id))
-    )
-    await User.updateOne({ _id: userId }, { games: filteredGames })
-  }
-
+  if (!(await isAdmin(req)))
+    await isDeleteGameAuthorized(decodedToken, req.params.id)
+  await deleteGame(req.params.id)
   res.status(204).end()
 })
 
 router.delete('/', async (req, res) => {
-  if (config.NODE_ENV !== 'test') return
-  await Game.deleteMany({})
+  adminRequired(req)
+  await deleteAllGames(req.body.adminSecret)
   res.status(204).end()
 })
 
---
+++ services/gameService.js
@@ -0,0 +1,166 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+const User = require('../models/user')
+const Game = require('../models/game')
+
+const getGames = async (params) => {
+  const games = Game.find(params)
+    .populate('players', {
+      games: 0,
+      admin: 0,
+    })
+    .populate('sport')
+    .populate('winners', { games: 0, admin: 0 })
+    .populate('submitter', { games: 0, admin: 0 })
+  return games
+}
+
+const validateMatchPlayers = async (data, submitter) => {
+  const players = {}
+  if (!data.players.includes(submitter.id)) {
+    throw {
+      name: 'Forbidden',
+      message: 'You can only post games where you participated',
+    }
+  }
+  for await (const [index, player] of data.players.entries()) {
+    if (player in players) {
+      throw {
+        name: 'Custom',
+        message: 'duplicate player',
+      }
+    }
+    players[player] = await User.findById(player)
+    if (players[player] === null) {
+      throw {
+        name: 'Custom',
+        message: `player ${index + 1} not in database`,
+      }
+    }
+  }
+  return players
+}
+
+const getWinners = async (data) => {
+  if (data.rounds.length === 0)
+    throw {
+      name: 'ValidationError',
+      message: 'can not save empty match',
+    }
+  const totalScores = Array(data.players.length).fill(0)
+
+  // Check rounds(s)
+
+  for (const [roundIndex, round] of data.rounds.entries()) {
+    if (round.length !== data.players.length) {
+      throw {
+        name: 'Custom',
+        message: `Incorrect amount of players in round ${roundIndex + 1}`,
+      }
+    }
+    for (const [scoreIndex, score] of data.rounds[roundIndex].entries()) {
+      if (!isNaN(score)) totalScores[scoreIndex] += Number(score)
+      else
+        throw {
+          name: 'Custom',
+          message: `malformatted score round ${roundIndex + 1} player ${
+            scoreIndex + 1
+          }`,
+        }
+    }
+  }
+  const winners = []
+  const maxScore = totalScores.reduce((a, b) => Math.max(a, b), 0)
+  for (const [index, value] of totalScores.entries()) {
+    if (value === maxScore) {
+      winners.push(data.players[index])
+    }
+  }
+  return winners
+}
+
+const updateParticipatingUsers = async (players, savedGame) => {
+  for (let userId of Object.keys(players)) {
+    const user = await User.findOne({ _id: userId })
+    if (user['games']) {
+      await User.findByIdAndUpdate(
+        userId,
+        {
+          games: [...user['games'], savedGame['_id']],
+        },
+        { runValidators: true, context: 'query' }
+      )
+    } else {
+      await User.findByIdAndUpdate(
+        userId,
+        { games: [savedGame['_id']] },
+        { runValidators: true, context: 'query' }
+      )
+    }
+  }
+}
+
+const addGame = async (data, submitter) => {
+  const players = await validateMatchPlayers(data, submitter)
+  const winners = await getWinners(data)
+  const newGame = new Game({
+    players: data['players'],
+    rounds: data['rounds'],
+    sport: data['sport'],
+    winners: winners,
+    date: Date.now(),
+    approved: data['players'].length > 1 ? false : true,
+    submitter: mongoose.Types.ObjectId(submitter.id),
+    approvedBy: mongoose.Types.ObjectId(submitter.id),
+  })
+
+  const savedGame = await newGame.save()
+  if (savedGame) {
+    await updateParticipatingUsers(players, savedGame)
+  }
+  return savedGame
+}
+
+const isDeleteGameAuthorized = async (decodedToken, id) => {
+  const game = await Game.findById(id)
+  if (game.submitter.toString() !== decodedToken.id)
+    throw {
+      name: 'Authorization',
+      message: 'You did not submit this game',
+    }
+  return true
+}
+
+const deleteGame = async (id) => {
+  const deletedMatch = await Game.findByIdAndDelete(id)
+  const players = deletedMatch.players
+
+  for await (const userId of players) {
+    const user = await User.findById(userId)
+    if (!user.games) continue
+    const filteredGames = user.games.filter(
+      (game) => !game.equals(mongoose.Types.ObjectId(id))
+    )
+    await User.updateOne({ _id: userId }, { games: filteredGames })
+  }
+}
+
+const deleteAllGames = async (adminSecret) => {
+  if (adminSecret !== config.NEW_ADMIN_SECRET)
+    throw {
+      name: 'Authorization',
+      message: 'unauthorized',
+    }
+  const games = await Game.find({})
+  for (const g of games) {
+    await deleteGame(g._id)
+  }
+}
+
+module.exports = {
+  getGames,
+  addGame,
+  isDeleteGameAuthorized,
+  deleteGame,
+  deleteAllGames,
+}
