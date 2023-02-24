c66618b3788715aae6a1fa8dd5d682dfa5fd385d
Desipeli <mr.desipeli@gmail.com>
2023-02-24 14:53:44 +0200

    tests working again. Only testing users

--- tests/users_api.test.js
+++ tests/users_api.test.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const { createUser, deleteAllUsers } = require('../services/userService')
 const app = require('../app')
+const config = require('../utils/config')
 const api = supertest(app)
 
 const userListLength = async (length) => {
@@ -9,30 +11,30 @@ const userListLength = async (length) => {
 }
 
 const createUsers = async () => {
+  const admin = {
+    username: 'admin',
+    password: 'Nolle!234',
+  }
   const user1 = {
     username: 'user1',
-    password: 'pass1',
+    password: 'Ykko!234',
   }
   const user2 = {
     username: 'user2',
-    password: 'pass2',
+    password: 'Kakko!234',
   }
   const user3 = {
     username: 'user3',
-    password: 'pass3',
+    password: 'Kolmo!234',
   }
-  const user4 = {
-    username: 'deleted',
-    password: 'pass4',
-  }
-  await api.post('/api/users').send(user1).expect(201)
-  await api.post('/api/users').send(user2).expect(201)
-  await api.post('/api/users').send(user3).expect(201)
-  await api.post('/api/users').send(user4).expect(201)
+  await createUser(admin.username, admin.password, '', true)
+  await createUser(user1.username, user1.password, '', false)
+  await createUser(user2.username, user2.password, '', false)
+  await createUser(user3.username, user3.password, '', false)
 }
 
-beforeAll(async () => {
-  await api.delete('/api/users')
+beforeEach(async () => {
+  await deleteAllUsers(config.NEW_ADMIN_SECRET)
 })
 
 test('userlist is empty', async () => {
@@ -40,14 +42,10 @@ test('userlist is empty', async () => {
 })
 
 describe('Create user', () => {
-  beforeEach(async () => {
-    await api.delete('/api/users')
-  })
-
   test('with punctuations and scandic letters in username & password', async () => {
     const newUser = {
       username: 'ÄöÖÅå!?,.',
-      password: 'åÅÄöÖ?!.,',
+      password: 'åÅ4ÄöÖ?!.,',
     }
     await api.post('/api/users').send(newUser).expect(201)
     await userListLength(1)
@@ -56,7 +54,7 @@ describe('Create user', () => {
   test('with min length username & password', async () => {
     const newUser = {
       username: '123',
-      password: '12345',
+      password: 'Test!234',
     }
     await api.post('/api/users').send(newUser).expect(201)
     await userListLength(1)
@@ -65,7 +63,7 @@ describe('Create user', () => {
   test('with max length username & password', async () => {
     const newUser = {
       username: '123456789012',
-      password: '12345678901234567890123456789012345678901234567890',
+      password: '12345678901234567890123456789012345678901234567aA!',
     }
     await api.post('/api/users').send(newUser).expect(201)
     await userListLength(1)
@@ -73,10 +71,6 @@ describe('Create user', () => {
 })
 
 describe('Can not create user', () => {
-  beforeEach(async () => {
-    await api.delete('/api/users')
-  })
-
   test('without username', async () => {
     const newUser = {
       username: '',
@@ -98,7 +92,7 @@ describe('Can not create user', () => {
   test('with too short username', async () => {
     const newUser = {
       username: 'ab',
-      password: 'abcde',
+      password: 'abc!A232de',
     }
     await api.post('/api/users').send(newUser).expect(400)
     await userListLength(0)
@@ -107,7 +101,7 @@ describe('Can not create user', () => {
   test('with too short password', async () => {
     const newUser = {
       username: 'abcde',
-      password: 'abcd',
+      password: 'A1!4d',
     }
     await api.post('/api/users').send(newUser).expect(400)
     await userListLength(0)
@@ -116,7 +110,7 @@ describe('Can not create user', () => {
   test('with too long username', async () => {
     const newUser = {
       username: '12345678901234',
-      password: 'abcde',
+      password: 'abc!A232de',
     }
     await api.post('/api/users').send(newUser).expect(400)
     await userListLength(0)
@@ -125,7 +119,7 @@ describe('Can not create user', () => {
   test('with too long password', async () => {
     const newUser = {
       username: '12345',
-      password: '123456789012345678901234567890123456789012345678906',
+      password: '123456789012345678901234567890123456789012345678aA!',
     }
     await api.post('/api/users').send(newUser).expect(400)
     await userListLength(0)
@@ -134,13 +128,13 @@ describe('Can not create user', () => {
   test('with existing username', async () => {
     const newUser = {
       username: '12345',
-      password: 'abcde',
+      password: '!AJE234sfe93c',
     }
     await api.post('/api/users').send(newUser).expect(201)
 
     const newUser2 = {
       username: '12345',
-      password: 'abcde',
+      password: '!AJE234sfe93c',
     }
     await api.post('/api/users').send(newUser2).expect(400)
     await userListLength(1)
@@ -149,14 +143,14 @@ describe('Can not create user', () => {
   test('with existing email', async () => {
     const newUser = {
       username: '12345',
-      password: 'abcde',
+      password: '!AJE234sfe93c',
       email: 'moi@moi.moi',
     }
     await api.post('/api/users').send(newUser).expect(201)
 
     const newUser2 = {
       username: 'abcdefg',
-      password: 'abcde',
+      password: '!AJE234sfe93c',
       email: 'moi@moi.moi',
     }
     await api.post('/api/users').send(newUser2).expect(400)
@@ -165,7 +159,7 @@ describe('Can not create user', () => {
 })
 
 describe('Find user', () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await createUsers()
   })
 
@@ -180,80 +174,72 @@ describe('Find user', () => {
       .post('/api/users')
       .send({
         username: 'newnew',
-        password: 'pewpew',
+        password: 'pewpew123AVB!!!',
       })
       .expect(201)
     const getUser = await api.get(`/api/users/?id=${newnew._id}`)
     expect(getUser.body.length === 1)
     expect(getUser.body.username === 'newnew')
   })
-
-  afterEach(async () => {
-    await api.delete('/api/users')
-  })
 })
 
 describe('Delete', () => {
   beforeEach(async () => {
-    await api.delete('/api/users').expect(204)
-    await api.delete('/api/games').expect(204)
-    await api.delete('/api/sports').expect(204)
     await createUsers()
   })
 
-  test('user with id', async () => {
+  test('own user with id', async () => {
+    const login = await api
+      .post('/api/login')
+      .send({ username: 'user3', password: 'Kolmo!234' })
     const res = await api.get('/api/users/?username=user3').expect(200)
     const _id = res.body[0]._id
-    await api.delete(`/api/users/${_id}`).expect(204)
+    await api
+      .delete(`/api/users/${_id}`)
+      .set('Authorization', `bearer ${login.body.token}`)
+      .expect(204)
     await api.get('/api/users').expect(200)
     await userListLength(3)
   })
 
-  test('deleted user id will be replaced with deleted-user in games', async () => {
-    const user1 = await api.get('/api/users/?username=user1').expect(200)
-    const user2 = await api.get('/api/users/?username=user2').expect(200)
-    const p1 = user1.body[0]._id
-    const p2 = user2.body[0]._id
+  test('other user by admin', async () => {
     const login = await api
       .post('/api/login')
-      .send({ username: 'user1', password: 'pass1' })
-    const token = `bearer ${login.body.token}`
-    await api.delete('/api/sports').expect(204)
-    const sport = await api
-      .post('/api/sports')
-      .send({ name: 'squash' })
-      .expect(201)
-    const newGame = {
-      players: [p1, p2],
-      rounds: [
-        [1, 2],
-        [3, 2],
-        [3, 2],
-      ],
-      sport: sport.body._id,
-    }
-    const savedGame = await api
-      .post('/api/games')
-      .set('Authorization', token)
-      .send(newGame)
-      .expect(201)
-    const gamesList = await api.get('/api/games').expect(200)
-    expect(gamesList.body).toHaveLength(1)
+      .send({ username: 'admin', password: 'Nolle!234' })
 
-    // Delete user and check
-    await api.delete(`/api/users/${p1}`).expect(204)
-    const replaced = await api.get('/api/users/?username=deleted')
-    const savedGameAfter = await api.get(`/api/games/${savedGame.body._id}`)
-    const deletedPlayer = savedGameAfter.body.players.filter(
-      (p) => p._id === replaced.body[0]._id
-    )
-    expect(deletedPlayer[0]._id).toBe(replaced.body[0]._id)
+    const res = await api.get('/api/users/?username=user3').expect(200)
+    const _id = res.body[0]._id
+    await api
+      .delete(`/api/users/${_id}`)
+      .set('Authorization', `bearer ${login.body.token}`)
+      .expect(204)
+    await api.get('/api/users').expect(200)
+    await userListLength(3)
+  })
+})
+
+describe('Can not delete', () => {
+  beforeEach(async () => {
+    await createUsers()
+  })
+
+  test('other user if not admin', async () => {
+    const login = await api
+      .post('/api/login')
+      .send({ username: 'user3', password: 'Kolmo!234' })
+
+    const res = await api.get('/api/users/?username=user2').expect(200)
+    const _id = res.body[0]._id
+    await api
+      .delete(`/api/users/${_id}`)
+      .set('Authorization', `bearer ${login.body.token}`)
+      .expect(401)
+    await api.get('/api/users').expect(200)
+    await userListLength(4)
   })
 })
 
 afterAll(async () => {
-  await api.delete('/api/users').expect(204)
-  await api.delete('/api/games').expect(204)
-  await api.delete('/api/sports').expect(204)
+  await deleteAllUsers(config.NEW_ADMIN_SECRET)
   await mongoose.connection.close()
 })
