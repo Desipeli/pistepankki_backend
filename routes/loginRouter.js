2cd67ea3d4628a075603406161fcb67a23a6dbbb
Desipeli <mr.desipeli@gmail.com>
2023-02-24 23:25:46 +0200

    github actionstest

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -5,29 +5,34 @@ const router = express.Router()
 const jwt = require('jsonwebtoken')
 
 router.post('/', async (req, res) => {
+  console.log('ALKAAAAAAAAAAAAAAAAAA')
   const { username, password } = req.body
   if (!(username && password) || username === 'deleted')
     throw {
       name: 'Authorization',
       message: 'provide username and password',
     }
+  console.log('TIEDOOOOT', username)
   const user = await User.findOne({ username })
 
+  console.log('UUUUSEERRR', user.username)
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordhash)
-
+  console.log('HASHATTUU')
   if (!(user && passwordCorrect))
     throw {
       name: 'Authorization',
       message: 'invalid credentials',
     }
-
   const tokenUser = {
     username: user.username,
     id: user._id,
   }
-
-  const token = jwt.sign(tokenUser, process.env.SECRET, { expiresIn: '90d' })
+  console.log('TOKKEEEN LUOTU')
+  const token = await jwt.sign(tokenUser, process.env.SECRET, {
+    expiresIn: '90d',
+  })
+  console.log('VALMIS!')
   res.status(200).send({ token, username: user.username })
 })
 
